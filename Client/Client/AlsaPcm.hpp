273d91a4eb95e09c5b662c5f99dc159fab434436
void <git.email.address@googlemail.com>
2014-10-06 22:11:32 +0200

    Wow, I'm terrible at C++

--- Client/Client/AlsaPcm.hpp
+++ Client/Client/AlsaPcm.hpp
@@ -14,15 +14,60 @@ public:
 	// Sample rate in Hz
 	// Latency in milliseconds
 	// Buffer length in milliseconds
-	AlsaPcm(const std::string & deviceName, snd_pcm_format_t format, unsigned sampleRate, unsigned latency, unsigned bufferLength);
-	~AlsaPcm();
-	
-	void open();
-	void close();
-	
-	void read();
-	
-	const SampleType * getBuffer(std::size_t & frameCount);
+	AlsaPcm(const std::string & deviceName, snd_pcm_format_t format, unsigned sampleRate, unsigned latency, unsigned bufferLength):
+		_deviceName(deviceName),
+		_format(format),
+		_sampleRate(sampleRate),
+		_latency(latency),
+		_bufferLength(bufferLength),
+		_pcm(nullptr),
+		_buffer(nullptr)
+	{
+		_frameCount = static_cast<std::size_t>((sampleRate * bufferLength) / 1000);
+	}
+
+	~AlsaPcm()
+	{
+		close();
+	}
+
+	void open()
+	{
+		if(_pcm != nullptr)
+			throw new Fall::Exception("ALSA PCM has already been opened");
+		// Use blocking read/write mode
+		int openMode = 0;
+		int error = snd_pcm_open(&_pcm, _deviceName.c_str(), SND_PCM_STREAM_CAPTURE, openMode);
+		errorCheck("snd_pcm_open", error);
+		// Mono recording
+		unsigned channelCount = 1;
+		// Disable software resampling
+		int enableSoftwareResampling = 0;
+		unsigned latencyMicroseconds = _latency * 1000;
+		error = snd_pcm_set_params(_pcm, _format, SND_PCM_ACCESS_RW_INTERLEAVED, channelCount, _sampleRate, enableSoftwareResampling, latencyMicroseconds);
+		errorCheck("snd_pcm_set_params", error);
+		_buffer = new SampleType[_frameCount];
+	}
+
+	void close()
+	{
+		if(_buffer != nullptr)
+			delete[] _buffer;
+		if(_pcm != nullptr)
+			snd_pcm_close(_pcm);
+	}
+
+	void read()
+	{
+		int error = snd_pcm_readi(_pcm, _buffer, static_cast<snd_pcm_uframes_t>(_frameCount));
+		errorCheck("snd_pcm_readi", error);
+	}
+
+	const SampleType * getBuffer(std::size_t & frameCount)
+	{
+		frameCount = _frameCount;
+		return _buffer;
+	}
 	
 private:
 	std::string _deviceName;
@@ -35,5 +80,9 @@ private:
 	snd_pcm_t * _pcm;
 	SampleType * _buffer;
 	
-	void errorCheck(std::string const & functionName, int error);
+	void errorCheck(const std::string & functionName, int error)
+	{
+		if(error < 0)
+			throw AlsaException(functionName, error);
+	}
 };
